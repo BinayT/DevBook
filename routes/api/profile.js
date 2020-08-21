2da70e9c18f6aea10683f7280a6c3d8489298772
Binay <binayprogramming@gmail.com>
2020-08-21 12:40:30 +0200

    Created and updated Profile Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -15,7 +15,7 @@ router.get("/test", (req, res) =>
   res.json({ msg: "Heya you're on route /api/profile/test" })
 );
 
-// @GET /api/profile/profile
+// @GET /api/profile
 // @desc Shows the current user's profile
 // @access Private
 router.get(
@@ -33,6 +33,63 @@ router.get(
         res.json(profile);
       })
       .catch((err) => res.status(404).json(err));
+  }
+);
+
+// @POST /api/profile
+// @desc Create or Edit User Profile
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Get all Fields
+    const profileFields = {};
+    const errors = {};
+
+    profileFields.user = req.body.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    //Skills - Split to arrays
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    //Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      if (profile) {
+        //If profile already exists, then UPDATE
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: True }
+        ).then((profile) => res.json(profile));
+      } else {
+        //If profile doesn't exist yet, then CREATE
+
+        //Check if handel exists
+        Profile.findOne({ handle: profileFields.handle }).then((profile) => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+          //Save profile
+          new Profile(profileFields).save().then((el) => res.json(el));
+        });
+      }
+    });
   }
 );
 
